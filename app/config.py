264e12cb1c8813baa7ff09f71a3ad44726d407e7
Abogo <abogonono1@gmail.com>
2024-09-06 01:08:51 +0100

    config restructured 	modified:   app/config.py

--- app/config.py
+++ app/config.py
@@ -1,12 +1,8 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_cors import CORS
+import os
 
-app = Flask(__name__)
-
-CORS(app)
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
-
-db = SQLAlchemy(app)
+class Config:
+    SECRET_KEY = os.getenv('SECRET_KEY', 'your_secret_key_here')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///school.db')
+    # Use SQLite for local development
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'your_jwt_secret_key')
